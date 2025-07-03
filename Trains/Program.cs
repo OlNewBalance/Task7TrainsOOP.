cf318b3a14a8c657f8ab5ed55096153f7b117e4a
Oleg <finkton23@gmail.com>
2025-07-03 16:30:40 +0500

    Update Program.cs

--- Trains/Program.cs
+++ Trains/Program.cs
@@ -1,3 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+namespace TrainSystem
+{
+    internal class Program ()
+    {
+        static public void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Чтобы войти в систему управления поездами, нажмите ENTER.");
+                if (Console.ReadLine() == "")
+                {
+                    TrainSystem ts = new TrainSystem();
+                    Console.WriteLine("Добро пожаловать! Введите сначала город отправления, затем город прибытия! Нажмите ENTER.");
+                    
+                    string inputCities = Console.ReadLine();
+                    string[] words = inputCities.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    
+                    if (words.Length == 2)
+                    {
+                        string firstCity = words[0];
+                        string secondCity = words[1];
+                        ts.TrainLine(firstCity, secondCity);
+                        
+                        Console.WriteLine("Нажмите ENTER чтобы начать продажу билетов.");
+                        if (Console.ReadLine() == "")
+                        {
+                            ts.TicketSale();
+                            
+                            Console.WriteLine("Нажмите ENTER чтобы начать формирование состава.");
+                            if (Console.ReadLine() == "")
+                            {
+                                ts.TrainForming();
+                                
+                                Console.WriteLine("Нажмите ENTER чтобы начать отправку состава.");
+                                if (Console.ReadLine() == "")
+                                {
+                                    ts.TrainSending();
+                                    Console.WriteLine("Нажмите ENTER чтобы начать заново.");
+                                    Console.ReadLine(); // Ожидание перед restart
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нужно ввести ровно два города!");
+                    }
+                }
+            }
+        }
+    }
+}
