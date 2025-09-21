119cbdb15dceabf536a1ae46a1108db5c36ca900
Oleg <finkton23@gmail.com>
2025-09-21 20:51:05 +0500

    Update TrainSystem.cs

--- Trains/TrainSystem.cs
+++ Trains/TrainSystem.cs
@@ -2,22 +2,22 @@ namespace TrainSystem;
 
 public class TrainSystem
 {
-    public List<Van> RowOfWagons = new List<Van>();
-    private bool trainLineIsReady = false;
-    private int countOfPassengers = 0;
-    private string point1;
-    private string point2;
+    internal string City1;
+    internal string City2;
+    internal bool trainLineIsReady = false;
+    internal int countOfPassengers { get; set; }
     Random random = new Random();
+    
     public void TrainLine(string point1, string point2)
     {
         Console.WriteLine($"Метод TrainLine вызван с параметрами: {point1}, {point2}"); // Отладочное сообщение
     
         if (point1 != null && point2 != null)
         {
-            this.point1 = point1;
-            this.point2 = point2;
+            City1 = point1;
+            City2 = point2;
             trainLineIsReady = true;
-            Console.WriteLine($"Путь {point1} - {point2} сформирован!");
+            Console.WriteLine($"Путь {City1} - {City2} сформирован!");
         }
         else
         {
@@ -31,52 +31,6 @@ public class TrainSystem
         {
             countOfPassengers = random.Next(1, 100);
             Console.WriteLine($"Билеты в количестве - {countOfPassengers} распроданы!");
-        }
-    }
-
-    public void TrainForming()
-    {
-        if (trainLineIsReady == true && countOfPassengers > 0)
-        {
-            if (countOfPassengers < 30)
-            {
-                RowOfWagons.Add(new Van(30,1));
-                Console.WriteLine($"Состав из 1-го вагона вместительностью 30 человек готов!");
-            }
-
-            if (countOfPassengers < 40 && countOfPassengers > 39)
-            {
-                RowOfWagons.Add(new Van(40, 2));
-                Console.WriteLine($"Состав из 1-го вагона вместительностью 40 человек готов!");
-            }
-
-            if (countOfPassengers < 60 && countOfPassengers > 49)
-            {
-                RowOfWagons.Add(new Van(60, 3));
-                Console.WriteLine($"Состав из 1-го вагона вместительностью 60 человек готов!");
-            }
-            else if (countOfPassengers > 60 && countOfPassengers > 49)
-            {
-                RowOfWagons.Add(new Van(60, 3));
-                RowOfWagons.Add(new Van(40, 2));
-                Console.WriteLine($"Состав из 2-х вагонов вместительностью 60(1) и 40(2) человек готов!");
-            }
-            
-        }
-    }
-
-    public void TrainSending()
-    {
-        if (trainLineIsReady == true && countOfPassengers > 0 && RowOfWagons != null)
-        {
-            foreach (Van van in RowOfWagons)
-            {
-               Console.WriteLine($"Поезд в {countOfPassengers} человек, с - {van.VanID} вагоном(ами) успешно -" +
-                                 $" отправлен из {point1} в {point2}!");
-               RowOfWagons.RemoveAll(_ => true); 
-            }
-            
-            
         }
     }
 }
